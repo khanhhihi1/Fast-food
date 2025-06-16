8e56f0f6e52860c7ea9d10f07749c69cc94c3624
khanhhihi1 <dongkhanh88888@gmail.com>
2025-06-16 23:52:25 +0700

    khanh da cap nhat api category

--- Back-end/myapp/model/categoriesModel.js
+++ Back-end/myapp/model/categoriesModel.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const categoriesSchema = new Schema({
   name: { type: String, required: true },
-  imageUrl: { type: String, required: true },
+  imageUrl: { type: String },
 });
 
 module.exports =
