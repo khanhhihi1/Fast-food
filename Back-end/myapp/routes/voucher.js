3224cc95443a506afe7584459b41f99a105a516f
khanhhihi1 <dongkhanh88888@gmail.com>
2025-07-08 23:21:25 +0700

    cap nhat oder

--- Back-end/myapp/routes/voucher.js
+++ Back-end/myapp/routes/voucher.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const voucherController = require("../controllers/voucherController.js");
+const voucherController = require("../controller/voucherController.js");
 
 router.get("/", voucherController.getAllVouchers);
 router.get("/:code", voucherController.getVoucherByCode);
 router.post("/", voucherController.createVoucher);
-
+router.post("/apply", voucherController.applyVoucher); 
 module.exports = router;
