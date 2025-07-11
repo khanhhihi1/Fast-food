2f3762a2f7edb79f1b5ccbda482ab6ef88202e3b
khanhhihi1 <dongkhanh88888@gmail.com>
2025-07-12 00:11:05 +0700

    da update them sua an voucher

--- Back-end/myapp/app.js
+++ Back-end/myapp/app.js
@@ -46,7 +46,7 @@ app.use("/cart", cartRouter);
 app.use("/orders", orderRouter);
 app.use("/voucher", voucherRouter);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
